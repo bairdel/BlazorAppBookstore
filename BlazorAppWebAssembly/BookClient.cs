55f036e379048784c3b03215e5fbe5e0e5f8ac31
bairdel <bairdel@outlook.com>
2023-10-27 16:47:26 +0100

    A Blazor WebAssembly with bootstrap?Add project files.

---
+++ BlazorAppWebAssembly/BookClient.cs
@@ -0,0 +1,45 @@
+﻿using BlazorAppWebAssembly.Models;
+
+namespace BlazorAppWebAssembly
+{
+    public static class BookClient
+    {
+        
+        public static Book[] GetBooks()
+        {
+            return books.ToArray();
+        }
+        
+        private static readonly List<Book> books = new() //Add on top: @using BookStore.Client.Models;
+            {
+        new Book()
+        {
+            ID = 1,
+            Name = "Java",
+            Genre = "Programming",
+            Price = 19.99M,
+            PublishDate = new DateTime(2022,2,10)
+        },
+        new Book()
+        {
+            ID = 2,
+            Name = "Data Science",
+            Genre = "AI",
+            Price = 39.99M,
+            PublishDate = new DateTime(2023, 2, 10)
+        },
+        new Book()
+        {
+            ID = 3,
+            Name = "Computer Systems",
+            Genre = "Hardware",
+            Price = 9.99M,
+            PublishDate = new DateTime(2021,2,10)
+        }
+    };
+
+
+
+
+    }
+}
---
+++ BlazorAppWebAssembly/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorAppWebAssembly.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public required string Name { get; set; }
+        public required string Genre { get; set; }
+        public decimal Price { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
